d671aaf23ad5de0cd1e1ba90faca8d839dcb7de7
bobagby78 <bobagby78@gmail.com>
2021-12-22 12:15:55 -0600

    Exerises completed with bonus mission

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidates.component.css']
 })
 export class CandidatesComponent implements OnInit {
-   missionName = "LaunchCode Moonshot"
+   missionName = "LaunchCode Moonshot";
+   editMissionName = false;
 
    candidates = [
     {name: 'Rusty Rutabaga', data: {age: 5, mass: '0.75 kg', sidekick: 'Blake'}, image: 'assets/images/Blake.png'},
@@ -25,10 +26,16 @@ export class CandidatesComponent implements OnInit {
 
   // Code the addToCrew function here:
   addToCrew(selectedCrew: object){
-    crew.push(selectedCrew);
+    if(!this.crew.includes(selectedCrew)){
+      this.crew.push(selectedCrew);
+    }
   }
 
   // BONUS: Code the changeMissionName function here:
+  changeMissionName(newMissionName){
+    this.missionName=newMissionName;
+    return false;
+  }
 
 
 }
